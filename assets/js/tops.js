40dedab2aa45e810ebca7ac8aee349b937757c74
Roy Magnussen <roymagnussen@hotmail.com>
2020-05-31 21:06:49 +0100

    Styled top.html

--- assets/js/tops.js
+++ assets/js/tops.js
@@ -36,7 +36,7 @@ const tops = [
 document.getElementById("products").innerHTML = tops.map((product) => {
   return `
   <div class="col-12 col-sm-6 col-md-4 col-lg-3 col-xl-2">
-  <div class="card card-body text-center">
+  <div class="card card-body text-center product-card">
     <img
       src="${product.image}"
       class="card-img"
