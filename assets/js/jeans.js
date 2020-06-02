34886d74bc62a74a98889440dec46c40852c391a
Roy Magnussen <roymagnussen@hotmail.com>
2020-06-02 17:03:22 +0100

    Create jeans page with search feature

---
+++ assets/js/jeans.js
@@ -0,0 +1,236 @@
+const jeans = [
+  {
+    productID: "1",
+    name: "Ankle Jeans",
+    price: "NT$1680",
+    category: "Jeans",
+    image: "/assets/images/clothing/jeans/blue-ankle-jeans-front.jpg",
+  },
+  {
+    productID: "2",
+    name: "Ripped knee Jeans",
+    price: "NT$1380",
+    category: "Jeans",
+    image: "/assets/images/clothing/jeans/blue-denim-jeans.jpg",
+    colour: "Light Blue",
+  },
+  {
+    productID: "2",
+    name: "Ripped turnup Jeans",
+    price: "NT$1680",
+    category: "Jeans",
+    image: "/assets/images/clothing/jeans/ripped-turnup-jeans-front.jpg",
+    colour: "Light Blue",
+  },
+];
+
+const search = document.getElementById("search");
+const data = document.getElementById("data");
+
+const searchJeans = async (searchText) => {
+  let matchedJeans = jeans.filter((product) => {
+    const regex = new RegExp(`${searchText}`, "gi");
+    return product.name.match(regex) || product.colour.match(regex);
+  });
+
+  if (searchText.length === 0) {
+    matchedJeans = [];
+    data.innerHTML = "";
+    $("#products").removeClass("d-none");
+  }
+
+  outputHtml(matchedJeans);
+};
+
+const outputHtml = (matchedJeans) => {
+  if (matchedJeans.length > 0) {
+    const html = matchedJeans
+      .map(
+        (product) => `
+    <div class="col-12 col-sm-6 col-md-4 col-lg-3 col-xl-2">
+  <div class="card card-body text-center product-card">
+    <img
+      src="${product.image}"
+      class="card-img"
+    />
+    <h4 class="mt-2">${product.name}</h4>
+    <p class="lead">${product.price}</p>
+    <button
+      type="button"
+      class="btn"
+      data-toggle="modal"
+      data-target="#top${product.productID}"
+    >
+      View
+    </button>
+  </div>
+  <!-- Modal -->
+  <div
+    class="modal fade"
+    id="top${product.productID}"
+    tabindex="-1"
+    role="dialog"
+    aria-hidden="true"
+  >
+    <div class="modal-dialog modal-dialog-centered" role="document">
+      <div class="modal-content">
+        <div class="modal-header">
+          <button
+            type="button"
+            class="close"
+            data-dismiss="modal"
+            aria-label="Close"
+          >
+            <span aria-hidden="true">&times;</span>
+          </button>
+        </div>
+        <div class="modal-body">
+          <div
+            id="carousel"
+            class="carousel slide"
+            data-ride="carousel"
+          >
+            <div class="carousel-inner">
+              <div class="carousel-item active">
+                <img
+                  class="d-block w-100"
+                  src="${product.image}"
+                />
+              </div>
+            </div>
+            <a
+              class="carousel-control-prev"
+              href="#carousel"
+              role="button"
+              data-slide="prev"
+            >
+              <span
+                class="carousel-control-prev-icon"
+                aria-hidden="true"
+              ></span>
+              <span class="sr-only">Previous</span>
+            </a>
+            <a
+              class="carousel-control-next"
+              href="#carousel"
+              role="button"
+              data-slide="next"
+            >
+              <span
+                class="carousel-control-next-icon"
+                aria-hidden="true"
+              ></span>
+              <span class="sr-only">Next</span>
+            </a>
+          </div>
+        </div>
+        <div class="modal-footer">
+          <h4 class="mr-auto">${product.name}</h4>
+          <p class="lead">${product.price}</p>
+        </div>
+      </div>
+    </div>
+  </div>
+</div>
+    `
+      )
+      .join("");
+
+    $("#products").addClass("d-none");
+    data.innerHTML = html;
+  }
+};
+
+search.addEventListener("input", () => searchJeans(search.value));
+
+document.getElementById("products").innerHTML = jeans
+  .map((product) => {
+    return `
+  <div class="col-12 col-sm-6 col-md-4 col-lg-3 col-xl-2">
+  <div class="card card-body text-center product-card">
+    <img
+      src="${product.image}"
+      class="card-img"
+    />
+    <h4 class="mt-2">${product.name}</h4>
+    <p class="lead">${product.price}</p>
+    <button
+      type="button"
+      class="btn"
+      data-toggle="modal"
+      data-target="#top${product.productID}"
+    >
+      View
+    </button>
+  </div>
+  <!-- Modal -->
+  <div
+    class="modal fade"
+    id="top${product.productID}"
+    tabindex="-1"
+    role="dialog"
+    aria-hidden="true"
+  >
+    <div class="modal-dialog modal-dialog-centered" role="document">
+      <div class="modal-content">
+        <div class="modal-header">
+          <button
+            type="button"
+            class="close"
+            data-dismiss="modal"
+            aria-label="Close"
+          >
+            <span aria-hidden="true">&times;</span>
+          </button>
+        </div>
+        <div class="modal-body">
+          <div
+            id="carousel"
+            class="carousel slide"
+            data-ride="carousel"
+          >
+            <div class="carousel-inner">
+              <div class="carousel-item active">
+                <img
+                  class="d-block w-100"
+                  src="${product.image}"
+                />
+              </div>
+            </div>
+            <a
+              class="carousel-control-prev"
+              href="#carousel"
+              role="button"
+              data-slide="prev"
+            >
+              <span
+                class="carousel-control-prev-icon"
+                aria-hidden="true"
+              ></span>
+              <span class="sr-only">Previous</span>
+            </a>
+            <a
+              class="carousel-control-next"
+              href="#carousel"
+              role="button"
+              data-slide="next"
+            >
+              <span
+                class="carousel-control-next-icon"
+                aria-hidden="true"
+              ></span>
+              <span class="sr-only">Next</span>
+            </a>
+          </div>
+        </div>
+        <div class="modal-footer">
+          <h4 class="mr-auto">${product.name}</h4>
+          <p class="lead">${product.price}</p>
+        </div>
+      </div>
+    </div>
+  </div>
+</div>
+  `;
+  })
+  .join("");
