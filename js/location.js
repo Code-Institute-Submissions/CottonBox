fcd82062ff8cc3068cbea59eaa4ade7bcebd6aba
Roy Magnussen <roysgamesandmore@gmail.com>
2020-06-17 23:21:50 +0100

    Change location url protocol

--- js/location.js
+++ js/location.js
@@ -1,7 +1,7 @@
 const access_key = "1bd0cdaafce863dcf36e31bf97cf8441";
 
 $.ajax({
-  url: "http://api.ipstack.com/check?access_key=" + access_key,
+  url: "https://api.ipstack.com/check?access_key=" + access_key,
   dataType: "jsonp",
   success: function (result) {
     if (result.country_code === "ZH" || result.country_code === "CN") {
