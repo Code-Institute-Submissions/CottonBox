10fb341b64824057334644c5e5ad186abffe9d5c
Roy Magnussen <roysgamesandmore@gmail.com>
2020-06-17 23:12:27 +0100

    Create ip location detection script

---
+++ js/location.js
@@ -0,0 +1,13 @@
+const access_key = "1bd0cdaafce863dcf36e31bf97cf8441";
+
+$.ajax({
+  url: "http://api.ipstack.com/check?access_key=" + access_key,
+  dataType: "jsonp",
+  success: function (result) {
+    if (result.country_code === "ZH" || result.country_code === "CN") {
+      window.location.href = "/zh/";
+    } else {
+      window.location.href = "index.html";
+    }
+  },
+});
